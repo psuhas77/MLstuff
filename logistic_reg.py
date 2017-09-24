7c933c56dc217110baa28ba141703b9b1b96bf2d
psuhas77 <31076903+psuhas77@users.noreply.github.com>
2017-09-25 00:28:09 +0530

    logistic regression is now completely modular

--- logistic_reg.py
+++ logistic_reg.py
@@ -1,9 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import scipy.optimize as opt
-
-
 
 
 
@@ -32,51 +29,25 @@ def grad_desc(X,Y,theta,alpha,iters):
     return theta,costarr
 
 
+def accuracy(X,Y,thetanew):
 
-path='C:\ML\ex2data1.txt'
+    finalar=[]
+    for i in X:
+        if i*(thetanew.T)>=0.5:
+            finalar.append(1)
 
-df=pd.read_csv(path,header=None,names=['exam1','exam2','pass'])
+        else:
+            finalar.append(0)
 
-passers=df.loc[df['pass']==1]
-failers=df.loc[df['pass']==0]
+    finalar=np.matrix(finalar).T
+    error=finalar-Y
+    numerror=0
+    for i in error:
+        if i!=0:
+            numerror+=1
 
-#plt.scatter(passers.exam1,passers.exam2,s=5,c='green')
-#plt.scatter(failers.exam1,failers.exam2,s=5,c='red',marker='v')
-#plt.show()
-
-df.insert(0,'ones',1)
-X=np.matrix(df.iloc[:,0:3])
-Y=np.matrix(df.iloc[:,3:])
-
-theta=np.matrix([0.0001,-0.0023,0.002])
-alpha=0.0001
-iters=100000
-
-print(theta.shape)
-print(X.shape)
-print(Y.shape)
-#print(cost_func(X,Y,theta))
-#thetanew,costarr=grad_desc(X,Y,theta,alpha,iters) 
-
-#X_ax=np.arange(iters)
-#plt.plot(X_ax,costarr)
-#plt.show()
-
-
-thetanew=np.matrix([-0.41526833,0.00872593,0.00719555])
-finalar=[]
-for i in X:
-    if i*(thetanew.T)>=0.5:
-        finalar.append(1)
-
-    else:
-        finalar.append(0)
-
-finalar=np.matrix(finalar).T
-error=finalar-Y
-numerror=0
-for i in error:
-    if i!=0:
-        numerror+=1
-
-print(numerror/(len(X)))
+    acc=numerror/(len(X))
+    return acc,finalar
+    
+    
+    
